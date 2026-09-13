5449d3c33e8d88c5890273849f8a082017fb146b
AnujRewar <anujrewar5@gmail.com>
2026-09-13 22:31:25 +0530

    Fix image paths for GitHub

--- template-devstarter/components/project-image.tsx
+++ template-devstarter/components/project-image.tsx
@@ -12,21 +12,28 @@ interface ProjectImageProps {
 export function ProjectImage({ src, alt }: ProjectImageProps) {
   const [loaded, setLoaded] = useState(false);
 
+  const imageSrc =
+      process.env.NODE_ENV === "production"
+          ? `/My-Portfolio-Website${src}`
+          : src;
+
   return (
-    <div
-      className={cn(
-        "bg-primary relative aspect-square overflow-hidden border-b border-border transition-opacity duration-500",
-        loaded ? "opacity-100" : "opacity-0"
-      )}
-    >
-      <Image
-        src={src}
-        alt={alt}
-        fill
-        onLoad={() => setLoaded(true)}
-        className="object-cover group-hover:scale-105 transition-transform duration-500 grayscale mix-blend-luminosity dark:mix-blend-darken"
-      />
-      <div className="absolute inset-0 bg-primary/5 opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
-    </div>
+      <div
+          className={cn(
+              "bg-primary relative aspect-square overflow-hidden border-b border-border transition-opacity duration-500",
+              loaded ? "opacity-100" : "opacity-0"
+          )}
+      >
+        <Image
+            src={imageSrc}
+            alt={alt}
+            fill
+            unoptimized
+            onLoad={() => setLoaded(true)}
+            className="object-cover group-hover:scale-105 transition-transform duration-500 grayscale mix-blend-luminosity dark:mix-blend-darken"
+        />
+
+        <div className="absolute inset-0 bg-primary/5 opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+      </div>
   );
 }
